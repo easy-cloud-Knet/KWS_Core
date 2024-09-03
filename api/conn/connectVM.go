47dd239ae2c799f0402b23c290bb135bc0265c00
kwonkwonn <ggiicchh48@gmail.com>
2024-09-03 07:43:53 -0400

    api for printing dom info added

--- api/conn/connectVM.go
+++ api/conn/connectVM.go
@@ -2,6 +2,7 @@ package conn
 
 import (
 	"fmt"
+	"log"
 
 	"libvirt.org/go/libvirt"
 )
@@ -9,21 +10,36 @@ import (
 
 
 
-func (i *InstHandler)ReturnDomainNameList(flag libvirt.ConnectListAllDomainsFlags) {
+func (i *InstHandler)ReturnDomainNameList(flag libvirt.ConnectListAllDomainsFlags)([]*DomainInfo,error) {
+	var Domains []*DomainInfo
 
 	doms, err := i.LibvirtInst.ListAllDomains(flag)
 	if err != nil {
 		panic(err)
 	}
+
 	fmt.Printf("%d running domains:\n", len(doms))
 	for _, dom := range doms {
-		name, err := dom.GetName()
-		if err == nil {
-			fmt.Printf("%s\n", name)
+		info, err := dom.GetInfo()
+		
+		if  err!=nil {
+				log.Println(err)
 		}
+		
+		DomInfo:= &DomainInfo{}
+		DomInfo.State = info.State
+		DomInfo.MaxMem = info.MaxMem
+		DomInfo.Memory = info.Memory
+		DomInfo.NrVirtCpu = info.NrVirtCpu
+		DomInfo.CpuTime = info.CpuTime
+		
+		Domains:=append(Domains, DomInfo)
+		Use(Domains)	
 		dom.Free()
 	}
-	
+	var erro1r error
+	erro1r=nil
+	return Domains,erro1r
 }
 
 
@@ -35,6 +51,15 @@ func (i *InstHandler)LibvirtConnection(){
 }
 	i.LibvirtInst = libvirtInst
 	defer i.InstMu.Unlock()
+}
+
+
+func Use(vals ...interface{}){
+	for _,val:= range vals{
+	 _=val
+	}
+	
+
 }
 
 
