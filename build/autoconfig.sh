3bab94c9617a3b0176d50e13b0bee6c104fa12b7
Kwon <ggiicchh48@gmail.com>
2024-09-05 19:04:59 +0900

    auto config added

--- build/autoconfig.sh
+++ build/autoconfig.sh
@@ -11,3 +11,13 @@ elif [[ $"shell_type" == "-zsh" ]]; then
 else 
     echo "hey"
 fi
+
+
+network_state=$(virsh net-list --all | awk '/default/ {print $2}')
+
+if [ "$network_state" != "active" ]; then
+    virsh net-autostart default
+    virsh net-start default
+else
+    echo "The default network is already active."
+fi
