be6e7a61a89f883970f2240c0cc61fbde1123a80
kwonkwonn <ggiicchh48@gmail.com>
2024-10-04 00:13:49 -0400

    needed file location changed

--- build/download.sh
+++ build/download.sh
@@ -4,6 +4,7 @@ sudo apt install -y  gcc
 sudo apt install -y  cloud-init genisoimage
 
 mkdir -p /var/lib/kws/baseimg
+mkdir /var/lib/kws/userConf
 
 sudo wget -O /var/lib/kws/baseimg/debian-12.7.0.qcow2  https://cloud.debian.org/images/cloud/bookworm/20240901-1857/debian-12-generic-amd64-20240901-1857.qcow2
 
