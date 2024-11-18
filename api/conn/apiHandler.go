6a134ed346ef79038457f9c36b83855dd997b779
root <root@vbox.myguest.virtualbox.org>
2024-11-18 21:24:26 +0900

    handler modified

--- api/conn/apiHandler.go
+++ api/conn/apiHandler.go
@@ -29,19 +29,20 @@ func (i * InstHandler) ReturnStatus(w http.ResponseWriter,r * http.Request){
 
 func (i *InstHandler) CreateDomainWithXML(w http.ResponseWriter, r *http.Request) {
 	// 파일 포인터를 슬라이스에 담습니다.
-	xmlConfig := `<domain type='kvm'>
+	xmlConfig := `
+	<domain type='kvm'>
   <name>cloud-vm</name>
   <uuid>6a21d302-e2b0-4a53-a9a5-4b08021cbba2</uuid>
   <memory unit='GiB'>2</memory>
   <vcpu placement='static'>2</vcpu>
+  <features>
+    <acpi/>
+  </features>
   <os>
-    <type arch='x86_64' >hvm</type>
+    <type arch='x86_64'>hvm</type>
     <boot dev='hd'/>
   </os>
   <devices>
-    <features>
-	  <acpi/>
-	</features>
     <emulator>/usr/bin/kvm</emulator>
     <disk type='file' device='disk'>
       <driver name='qemu' type='qcow2'/>
@@ -50,24 +51,23 @@ func (i *InstHandler) CreateDomainWithXML(w http.ResponseWriter, r *http.Request
     </disk>
     <disk type='file' device='cdrom'>
       <driver name='qemu' type='raw'/>
-      <source file='/var/lib/kws/userConf/user1/cidata.iso'/>
-      <target dev='hda' bus='ide'/>
+      <source file='/var/lib/kws/user1/cidata.iso'/>
+      <target dev='sda' bus='ide'/>
       <readonly/>
     </disk>
-   <serial type='pty'>
-		  <target port='0'/>
-		</serial>
-		<console type='pty'>
-		  <target type='serial' port='0'/>
-		</console>
-	<interface type='network'>
+    <serial type='pty'>
+      <target port='0'/>
+    </serial>
+    <console type='pty'>
+      <target type='serial' port='0'/>
+    </console>
+    <interface type='network'>
       <source network='default'/>
       <model type='virtio'/>
     </interface>
     <graphics type='vnc' port='-1' autoport='yes'/>
   </devices>
-</domain>
-	`
+</domain>	`
 	
 
 	// 추가 파일이 없는 경우 빈 슬라이스를 전달합니다.
