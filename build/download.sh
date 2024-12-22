dec54330d1dd0fb0b8119bf154ffd20c7c0f3ed5
root <root@vbox.myguest.virtualbox.org>
2024-12-22 20:16:16 +0900

    vm template updated

--- build/download.sh
+++ build/download.sh
@@ -8,4 +8,7 @@ mkdir /var/lib/kws/userConf
 
 sudo wget -O /var/lib/kws/baseimg/debian-12.7.0.qcow2  https://cloud.debian.org/images/cloud/bookworm/20240901-1857/debian-12-generic-amd64-20240901-1857.qcow2
 
+
+
+
 sudo wget -O /var/lib/kws/baseimg/ubuntu-cloud-24.04.img   https://cloud-images.ubuntu.com/noble/20240822/noble-server-cloudimg-amd64.img
