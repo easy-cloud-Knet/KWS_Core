b941608fe72b704fd671ee4090cf487a7604e270
kwonkwonn <kwonkwonn@gmail.com>
2024-12-24 15:21:43 +0900

    cloud-init configuration

--- build/autoGen.sh
+++ build/autoGen.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-DATA=$1
-
-echo "$DATA"
-
-qemu-img create -b /var/lib/kws/baseimg/ubuntu-cloud-24.04.img -f qcow2 -F qcow2 "./user${DATA}.qcow2"  10G
+METADATA=$1
 
 
+mkdir /var/lib/kws/${METADATA}
 
-genisoimage --output cidata.iso -V cidata -r -J user-data meta-data
+#making user-data meta-data here 
+
+qemu-img create -b /var/lib/kws/baseimg/ubuntu-cloud-24.04.img -f qcow2 -F qcow2 "/var/lib/kws/${METADATA}/${METADATA}.qcow2"  10G
+
+
+
+genisoimage --output /var/lib/kws/${METADATA}/cidata.iso -V cidata -r -J /var/lib/kws/${METADATA}/user-data /var/lib/kws/${METADATA}/meta-data
 
 
