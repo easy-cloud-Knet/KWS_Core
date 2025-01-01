7f28aecc8da166787f55927c85df376d4172237f
Kwon <ggiicchh48@gmail.com>
2025-01-01 17:28:33 +0900

    addressing passwd Encryption err

--- api/parsor/YAML_Parsor.go
+++ api/parsor/YAML_Parsor.go
@@ -1,6 +1,7 @@
 package parsor
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -11,14 +12,15 @@ import (
 func(u *User_data_yaml) Parse_data(param *VM_Init_Info){
 
 	Users_Detail:= []interface{}{"default",}
+
 	for _,User := range param.Users{
-		output, err:= exec.Command("openssl", "passwd", "-6", User.PassWord).Output()
+		outputPasswd, err:= PasswdEncryption(User.PassWord)
 		if err!= nil{
 			fmt.Println(err)
 		}
 		Users_Detail=append(Users_Detail, User_specific{
 			Name:User.Name,
-			Passwd:string(output),
+			Passwd:outputPasswd,
 			Groups: User.Groups,
 			Ssh_authorized_keys: []string{"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/ywMjVatnszunIy8axe43sMkzJum+Rw81UibQAID7xZouNNpDADNiQNicBW8dcuj44ScGnMZJpNmEYHgVrSCDDiC8uBC1NgzSpeURQwiSGrXZh0/sowmJaAm8cWHdvhHqFUHsIEIgSSh13iNAam2TAhajtU9MwPZreMNwNpN/qHqKHpq4FCXKn441gs7mE/VcPOj8pau6jM/9Bb8Wg9kmjhF3y1vN1YgKIXLdm0CW1x11axUKvKY7v1D7BaVL618×Md+e4zsLOCObHYw9KEsn7asOKcfUwLXScjWXNVUexv06+voltUdSA976NGHZIGZqEzvMttH+6TQVNSa78kIUls71N1A9v4yiqx"},
 			SuGroup: "ALL=(ALL) NOPASSWD:ALL",
@@ -49,4 +51,26 @@ func(u *User_data_yaml) Parse_data(param *VM_Init_Info){
 func (m* Meta_data_yaml) Parse_data(param *VM_Init_Info){
 	m.Instance_ID=param.UUID
 	m.Local_Host_Id=param.DomName
+}
+
+
+func PasswdEncryption (passwd string) (string ,error) {
+		cmd := exec.Command("mkpasswd", "--method=SHA-512", "--stdin")
+		
+		var stdin bytes.Buffer
+		stdin.WriteString(passwd)
+		cmd.Stdin = &stdin
+		
+		var stdout bytes.Buffer
+		cmd.Stdout = &stdout
+		
+		var stderr bytes.Buffer
+		cmd.Stderr = &stderr
+		
+		err := cmd.Run()
+		if err != nil {
+			return "", fmt.Errorf("failed to hash password: %v, stderr: %s", err, stderr.String())
+		}
+		
+		return stdout.String(), nil
 }
