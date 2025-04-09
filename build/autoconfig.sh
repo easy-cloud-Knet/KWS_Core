65950e6bc374151555932f7c9ca0a08345d443cf
kwonkwonn <kwonkwonn@gmail.com>
2025-04-09 17:33:40 +0900

    node exporter automation added in bulld/autoconf.sh

--- build/autoconfig.sh
+++ build/autoconfig.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
-# 현재 셸 타입을 확인하는 함수
 get_shell_type() {
-    ps -p $$ -o cmd= | awk '{print $1}'| xargs basename
+    ps -p $$ -o cmd= | awk '{print $1}' | xargs basename
 }
 
+curr_dir=$(pwd)
 shell_type=$(get_shell_type)
 
-# 셸에 따라 적절한 설정 파일을 수정
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar -xaf node_exporter-1.9.1.linux-amd64.tar.gz
+mv node_exporter-1.9.1.linux-amd64 node_exporter
+
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null << EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=root
+Group=root
+Type=simple
+ExecStart=${curr_dir}/node_exporter/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo rm node_exporter-1.9.1.linux-amd64.tar.gz
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter.service
+sudo systemctl start node_exporter.service
+
+# 셸 설정
 if [[ "$shell_type" == "bash" ]]; then
-    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
-    source ~/.bashrc
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+    echo "셸을 다시 시작하거나 'source ~/.bashrc'를 실행하세요."
 elif [[ "$shell_type" == "zsh" ]]; then
-    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
-    source ~/.zshrc
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+    echo "셸을 다시 시작하거나 'source ~/.zshrc'를 실행하세요."
 else
     echo "Unsupported shell: $shell_type"
 fi
 
-
-source ~/.bashrc
-mkdir /var/log/kws
