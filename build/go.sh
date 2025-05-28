c6473359ba0c7b31d4734fd349d83891e5279d49
kwonkwonn <ggiicchh48@gmail.com>
2025-05-28 17:33:22 +0900

    go path adding to sudo automation

--- build/go.sh
+++ build/go.sh
@@ -3,5 +3,6 @@ wget  https://go.dev/dl/go1.22.6.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.6.linux-amd64.tar.gz
 
 
+sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
 
 rm go1.22.6.linux-amd64.tar.gz
