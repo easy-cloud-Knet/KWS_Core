dfb302a408649f20a34630c5ecc11acb0c506f6d
kwonkwonn <ggiicchh48@gmail.com>
2025-06-29 09:17:51 +0000

    ovn bootstrap added

---
+++ build/sdnConf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt install make libssl-dev unbound -y autoconf automake libtool  
+git clone https://github.com/ovn-org/ovn.git
+cd ovn
+touch /dev/urandom
+touch /dev/net/tun
+
+
+sudo ./boot.sh
+
+#updating submodule from ovn github, can be replaced if advance feature of ovs are needed
+
+git submodule update --init
+cd ovs
+sudo ./boot.sh
+sudo ./configure
+sudo make
+sudo make install
+
+sudo .. 
+sudo ./configure
+sudo make
+sudo make install 
+
+echo "export PATH=$PATH:/usr/local/share/ovn/scripts">> ~/.bashrc  
+
+
+ovn-ctl start_controller
+
+
+
+
