b38f392266a9fa1f844d8b24dff577b94649b9d8
kwonkwonn <ggiicchh48@gmail.com>
2025-07-06 18:08:02 +0000

    ovn-clustering config added

---
+++ build/ovn-worker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+sudo mkdir -p /usr/local/etc/ovn
+sudo mkdir -p /usr/local/var/run/ovn
+sudo mkdir -p /usr/local/var/log/ovn
+sudo mkdir -p /usr/local/var/log/openvswitch/
+
+
+
+sudo ovs-ctl start 
+
+
+IP_ADDRESS=$1
+DNS=$2
+FILE_PATH="/etc/systemd/network/20-br-ext.network"
+
+
+cat <<EOF | sudo tee "$FILE_PATH" > /dev/null
+[Match]
+Name=br-ext
+
+[Network]
+Address=$IP_ADDRESS/24
+DNS=$DNS
+Gateway=8.8.8.8
+EOF
+
+sudo ovs-vsctl add-br br-ext
+sudo systemctl restart systemd-networkd 
+sudo ovs-vsctl add-port br-ext ens3 
+
+sudo ovs-vsctl set open_vswitch . external-ids:ovn-remote="tcp:10.5.15.39:6642" # SB DB는 10.5.15.42
+sudo ovs-vsctl set open_vswitch . external-ids:ovn-encap-type=geneve
+sudo ovs-vsctl set open_vswitch . external-ids:ovn-encap-ip=10.5.15.41 # 이 노드의 br-ext IP
+sudo ovs-vsctl set open_vswitch . external-ids:ovn-bridge-mappings=UPLINK:br-ext
+
+
+sudo ovn-ctl start_controller
+
