e14665043457394f5c3f512b4615ba36f690e3a1
Kwon <ggiicchh48@gmail.com>
2026-02-22 16:02:13 +0900

    update unrelated naming from DataDog

--- DomCon/domain_status/newStatus.go
+++ DomCon/domain_status/newStatus.go
@@ -5,40 +5,37 @@ import (
 	"libvirt.org/go/libvirt"
 )
 
-
-
 func NewDataDog(state libvirt.ConnectListAllDomainsFlags) DataDog {
 	switch state {
-		case libvirt.CONNECT_LIST_DOMAINS_ACTIVE:
-			return &libvirtStatus{}
-		case libvirt.CONNECT_LIST_DOMAINS_INACTIVE:
-			return &XMLStatus{}
-		default:
-			return nil
+	case libvirt.CONNECT_LIST_DOMAINS_ACTIVE:
+		return &libvirtStatus{}
+	case libvirt.CONNECT_LIST_DOMAINS_INACTIVE:
+		return &XMLStatus{}
+	default:
+		return nil
 	}
 }
 
-
-func (ds *XMLStatus) Retreive(dom *libvirt.Domain,DLS *DomainListStatus, logger zap.Logger) ( error) {
+func (ds *XMLStatus) UpdateStatus(dom *libvirt.Domain, DLS *DomainListStatus, logger zap.Logger) error {
 	domcnf, err := XMLUnparse(dom)
 	if err != nil {
 		logger.Error("failed to unparse domain XML", zap.Error(err))
-		return nil
+		return err
 	}
-		DLS.AddAllocatedCPU(int(domcnf.VCPU.Value))
-		DLS.AddSleepingCPU(int(domcnf.VCPU.Value))
-		return nil
+	DLS.AddAllocatedCPU(int(domcnf.VCPU.Value))
+	DLS.AddSleepingCPU(int(domcnf.VCPU.Value))
+	return nil
 
-}	
-	
-func (ls *libvirtStatus) Retreive(dom *libvirt.Domain, DLS *DomainListStatus, logger zap.Logger) (error) {
+}
+
+func (ls *libvirtStatus) UpdateStatus(dom *libvirt.Domain, DLS *DomainListStatus, logger zap.Logger) error {
 	cpuCount, err := dom.GetMaxVcpus()
 	if err != nil {
 		logger.Error("failed to get live vcpu count", zap.Error(err))
-		return nil
+		return err
 	}
 
 	DLS.AddAllocatedCPU(int(cpuCount))
 	return nil
-	
+
 }
