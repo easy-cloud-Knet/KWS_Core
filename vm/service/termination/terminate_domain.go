bb19cddf7317564369d1d9570ad678887241fbeb
Kwon <ggiicchh48@gmail.com>
2026-03-09 15:24:07 +0900

    split hard-coupled structures wiith interface for testability

--- vm/service/termination/terminate_domain.go
+++ vm/service/termination/terminate_domain.go
@@ -3,28 +3,24 @@ package termination
 import (
 	"fmt"
 
-	domCon "github.com/easy-cloud-Knet/KWS_Core/DomCon"
 	virerr "github.com/easy-cloud-Knet/KWS_Core/error"
-	"libvirt.org/go/libvirt"
 )
 
-func DomainTerminatorFactory(Domain *domCon.Domain) (*DomainTerminator, error) {
+func DomainTerminatorFactory(d Domain) DomainTermination {
 	return &DomainTerminator{
-		domain: Domain,
-	}, nil
+		domain: d,
+	}
 }
 
-func (DD *DomainTerminator) TerminateDomain() (*libvirt.Domain, error) {
-	dom := DD.domain
-
-	isRunning, err := dom.Domain.IsActive()
+func (DD *DomainTerminator) TerminateDomain() error {
+	isRunning, err := DD.domain.IsActive()
 	if !isRunning {
-		return nil, virerr.ErrorGen(virerr.DomainShutdownError, fmt.Errorf("error checking domain's aliveness, from libvirt. %w", err))
+		return virerr.ErrorGen(virerr.DomainShutdownError, fmt.Errorf("error checking domain's aliveness, from libvirt. %w", err))
 	}
 
-	if err := dom.Domain.Destroy(); err != nil {
-		return nil, virerr.ErrorGen(virerr.DomainShutdownError, fmt.Errorf("error shutting down domain, from libvirt. %w, %v", err, DD))
+	if err := DD.domain.Destroy(); err != nil {
+		return virerr.ErrorGen(virerr.DomainShutdownError, fmt.Errorf("error shutting down domain, from libvirt. %w", err))
 	}
 
-	return dom.Domain, nil
+	return nil
 }
