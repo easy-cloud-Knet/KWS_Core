eb0f66470bbefbf8325d6dfcb56cb123cc6977de
Kwon <ggiicchh48@gmail.com>
2026-03-23 21:00:22 +0900

    restore cmd

---
+++ cmd/doddle/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	_ "context"
+	_ "log"
+	"runtime/debug"
+
+	domCon "github.com/easy-cloud-Knet/KWS_Core/DomCon"
+	"github.com/easy-cloud-Knet/KWS_Core/api"
+	"github.com/easy-cloud-Knet/KWS_Core/internal/config"
+	syslogger "github.com/easy-cloud-Knet/KWS_Core/internal/logger"
+	"github.com/easy-cloud-Knet/KWS_Core/internal/server"
+	"go.uber.org/zap"
+)
+
+func main() {
+	debug.SetTraceback("none")
+	logger := syslogger.InitialLogger()
+
+	domListCon := domCon.DomListConGen()
+
+	libvirtInst := api.InstHandler{
+		DomainControl: domListCon,
+		Logger:        logger,
+	}
+
+	libvirtInst.LibvirtConnection()
+	libvirtInst.DomainControl.SetLibvirt(libvirtInst.LibvirtInst)
+	libvirtInst.DomainControl.DomainListStatus.Update()
+	if err := libvirtInst.DomainControl.RetrieveAllDomain(logger); err != nil {
+		logger.Fatal("failed to retrieve domains on startup", zap.Error(err))
+	}
+
+	go server.InitServer(config.ServerPort, &libvirtInst, logger)
+	defer func() {
+		logger.Info("Shutting down gracefully...") // 종료 시 로깅
+		logger.Sync()
+		libvirtInst.LibvirtInst.Close()
+	}()
+	select {}
+}
